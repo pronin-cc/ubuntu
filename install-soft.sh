84bfc03ad092fde0e1cbae7bf0b440621d25b0c6
Vitaly Pronin <r@pronin.cc>
2015-02-18 20:59:43 +0300

    Create install-soft.sh

---
+++ install-soft.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# For server
+apt-get install
+bash-completion
+mc
+nano
+nmap
+dput
+
+# For desktop
+apt-get install
+audacious
+audacity
+gimp
+inkscape
+skype
+agave
+mplayer
+smplayer
+vlc
+darktable
+filezilla
+remmina
+gdebi
+meld
+poedit
