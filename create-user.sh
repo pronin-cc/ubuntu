b0904d4c7b6c82d4eff835686b46e7a218adb9f3
Vitaly Pronin <r@pronin.cc>
2015-02-18 21:11:59 +0300

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Указываем список серверов, на которых необходимо создать учетные записи
 servers=('192.168.130.137')
+# Пользователь с правами sudo для подключения к удаленному серверу
+SUDO_USER=
 
 for i in ${servers[@]}
   do
     echo $i
-    ssh -t $i -l USER -p60123 'pass=test; name=test; sudo useradd -m -G sudo -s /bin/bash -U $name'
+    # Первый способ
+    ssh -t $i -l $SUDO_USER -p SSH_PORT 'name=USER; sudo useradd -m -G sudo -s /bin/bash -U $name'
+    # Второй способ
+    # ssh -t $i -l $SUDO_USER -p SSH_PORT 'name=USER; sudo adduser $name --ingroup sudo' 
+    echo '#####'
 done
+exit
